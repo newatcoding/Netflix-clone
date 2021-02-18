7698cfd45c4670f91df19a4386a29caff02ae901
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2021-02-19 03:24:16 +0530

    Netflix part 1

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA_eBAfnnPaIIRKzUjUoN-176ecSDiMyH4",
+    authDomain: "netflix-clone-96032.firebaseapp.com",
+    projectId: "netflix-clone-96032",
+    storageBucket: "netflix-clone-96032.appspot.com",
+    messagingSenderId: "529309830265",
+    appId: "1:529309830265:web:7adf915c11221a640498a8"
+  }; 
